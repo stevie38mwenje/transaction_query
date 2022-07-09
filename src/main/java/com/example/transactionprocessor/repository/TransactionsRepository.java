e15da5c83bd4aeead4c9e3146061d58c8a24cc06
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-07-10 01:15:47 +0300

    added date formatter on transaction entity

--- src/main/java/com/example/transactionprocessor/repository/TransactionsRepository.java
+++ src/main/java/com/example/transactionprocessor/repository/TransactionsRepository.java
@@ -11,9 +11,6 @@ import java.util.List;
 @Repository
 public interface TransactionsRepository extends JpaRepository<Transactions,Long> {
     List<Transactions> findTransactionsByUserId(Long userId);
-//    List<Transactions> findByDateRangeAndUserId(Date datefrom, Date dateto, Long userId);
-
-//    @Query(value = "from Transactions t where yourDate BETWEEN :from AND :to")
 
     List<Transactions> findByDateBetweenAndUserId(Long userId,Date from, Date to);
 }
