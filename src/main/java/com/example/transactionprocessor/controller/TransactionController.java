af843b7ebc3d16312b9ed94c1b07b093a3ea79e4
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-07-13 16:31:40 +0300

    added security module

--- src/main/java/com/example/transactionprocessor/controller/TransactionController.java
+++ src/main/java/com/example/transactionprocessor/controller/TransactionController.java
@@ -7,6 +7,8 @@ import com.example.transactionprocessor.model.Transactions;
 import com.example.transactionprocessor.service.TransactionService;
 //import com.example.transactionprocessor.service.UserService;
 //import com.example.transactionprocessor.utility.JWTUtility;
+import com.example.transactionprocessor.service.UserService;
+import com.example.transactionprocessor.utility.JWTUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 //import org.springframework.security.authentication.BadCredentialsException;
 //import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 //import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -27,35 +33,35 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RequestMapping("/transactions")
 public class TransactionController {
-//    @Autowired
-//    private JWTUtility jwtUtility;
+    @Autowired
+    private JWTUtility jwtUtility;
 
     @Autowired
     TransactionService transactionService;
 
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
 
 
-//    @PostMapping("/authenticate")
-//    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
-//        try {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),jwtRequest.getPassword()));
-//        } catch (BadCredentialsException e) {
-//            throw new Exception("INVALID_CREDENTIALS", e);
-//        }
-//
-//        final UserDetails userDetails
-//                = userService.loadUserByUsername(jwtRequest.getUsername());
-//
-//        final String token =
-//                jwtUtility.generateToken(userDetails);
-//
-//        return  new JwtResponse(token);
-//    }
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),jwtRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return  new JwtResponse(token);
+    }
 
     @GetMapping(value = "/{userId}")
     List<Transactions> getTransactions(@PathVariable(name = "userId") Long userId)
